8ca5bded7d519fc2826ed59261a86b2656e1b1b7
tournbr <tournbr@cronos.be>
2024-09-11 11:16:17 +0000

    Added UserMapperExtensions.cs

---
+++ TimesheetApp.API/Users/Mappers/UserMapperExtensions.cs
@@ -0,0 +1,19 @@
+@@ -1,18 +0,0 @@
+﻿using TimesheetApp.API.Users.Data.Entities;
+using TimesheetApp.API.Users.Models;
+
+namespace TimesheetApp.API.Users.Mappers;
+
+public static class UserMapperExtensions
+{
+    public static UserModel ToModel(this UserEntity user)
+    {
+        return new UserModel
+        {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email,
+        };
+    }
+}
